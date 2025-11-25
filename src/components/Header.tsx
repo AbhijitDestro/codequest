268a89f8ef2a39afa1cac94a842c33ee4291fd41
AbhijitDestro <abhijitdutta1806@gmail.com>
2025-11-25 12:57:54 +0530

    Started the landing page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,95 @@
-import React from 'react'
 import Image from 'next/image'
 import { Button } from './ui/button'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import Link from "next/link"
+import { Grid2X2 } from 'lucide-react'
+
+const courses =[
+  {
+     id:1,
+     name:"HTML",
+     desc:"Learn the fundamentals of HTML and build the structure of modern web pages",
+     path:"/courses/1/details",
+  },
+  {
+     id:2,
+     name:"CSS",
+     desc:"Learn the fundamentals of CSS and style modern web pages",
+     path:"/courses/2/details",
+  },
+  {
+     id:3,
+     name:"JavaScript",
+     desc:"Learn the fundamentals of JavaScript and build interactive web pages",
+     path:"/courses/3/details",
+  },
+  {
+     id:4,
+     name:"React",
+     desc:"Learn the fundamentals of React and build interactive web pages",
+     path:"/courses/4/details",
+  },
+  {
+     id:5,
+     name:"Node.js",
+     desc:"Learn the fundamentals of Node.js and build interactive web pages",
+     path:"/courses/5/details",
+  },
+]
 
 
 const Header = () => {
   return (
-    <div className="p-4 max-w-7xl">
-        <div className="flex items-center gap-2">
+    <div className="p-4 max-w-7xl flex items-center justify-between mx-auto">
+      <div className="flex items-center gap-2">
         <Image src={'/logo.png'} alt="logo" width={40} height={40} />
-        <h2 className="text-3xl font-bold ">CodeQuest</h2>
-        </div>
-        <div className="flex justify-end gap-4">
-        </div>
-        <div>
-            <Button variant={"outline"}>Sign In</Button>
-        </div>
+        <h2 className="text-4xl font-medium ">CodeQuest</h2>
+      </div>
+      <div className="flex items-center gap-2">
+        <NavigationMenu>
+          <NavigationMenuList className="gap-8">
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Courses</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid md:grid-cols-2 gap-2 sm:w-[400px] md:w-[500px] lg:w-[600px] p-4">
+                  {courses.map((course) => (
+                    <div key={course.id} className="p-2 hover:bg-accent rounded-xl cursor-pointer">
+                       <h2 className="font-medium">{course.name}</h2>
+                       <p className="text-sm text-gray-500">{course.desc}</p>
+                    </div>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink>
+                <Link href={'/projects'}>Projects</Link>  
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink>
+                <Link href={'/pricing'}>Pricing</Link>  
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink>
+                <Link href={'/contact'}>Contact Us</Link>  
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <div>
+        <Button variant={"outline"}>Sign In</Button>
+      </div>
     </div>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import Image from 'next/image'
+import { Button } from './ui/button'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="w-full relative h-screen overflow-hidden">
+      <Image src={'/hero.gif'} alt="hero" width={1000} height={1000} className="w-full h-full object-cover absolute inset-0" />
+      <div className="absolute w-full flex flex-col items-center justify-center py-28">
+        <h2 className="font-bold text-7xl font-game">Start Your</h2>
+        <h2 className="text-9xl font-bold font-game text-yellow-400" style={{textShadow: "3px 3px 0 #000, -3px -3px 0 #000, 3px -3px 0 #000, -3px 3px 0 #000" }}>Coding Journey</h2>
+        <h2 className="text-3xl font-medium font-game mt-5">Beginner friendly coding courses and projects</h2>
+        <Button className="mt-5">Get Started</Button>
+      </div>
+    </div>
   )
 }
 
